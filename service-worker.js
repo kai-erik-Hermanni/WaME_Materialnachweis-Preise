be2795b6eeac3554e30496fdd46f05f855a42adf
kai-erik-Hermanni <kai-erik@web.de>
2026-09-18 07:54:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='materialnachweis-preise-v1-59';
+const CACHE_NAME='materialnachweis-preise-v1-60';
 const ASSETS=["./", "./index.html", "./preise.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))}); 
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
